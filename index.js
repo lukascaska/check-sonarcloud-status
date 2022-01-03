2320783886eca8f8b4a0bd769a95ff1908ff28eb
Lucas <lucas.sales@paketa.com.br>
2022-01-03 08:09:11 -0300

    update action

--- index.js
+++ index.js
@@ -5,23 +5,20 @@ const { default: axios } = require('axios');
 try {
     const sonarToken = core.getInput('SONAR_TOKEN');
     const sonarProject = core.getInput('SONAR_PROJECT');
-    axios.get(`https://sonarcloud.io/api/project_pull_requests/list?project=${sonarProject}`, {
+    const sonarCloudData = await axios.get(`https://sonarcloud.io/api/project_pull_requests/list?project=${sonarProject}`, {
         headers: {
             Authorization: `Bearer ${sonarToken}`
         }
-    }).then((r) => {
-        const { pullRequests } = r.data
-        const prNumber = github.context.issue.number;
-        const PrDetailsPosition = pullRequests.findIndex(pr => pr.key === String(prNumber))
-        const PRDetails = pullRequests[PrDetailsPosition]
-        console.log(PRDetails.status.qualityGateStatus)
-        if (PRDetails.status.qualityGateStatus === "ERROR") {
-            throw new Error(`Sonarcloud status: FAILED, please check sonar cloud for more details`)
-        } 
-        console.log(`Sonarcloud status: SUCCESS`);
-    }).catch(error => {
-        throw error
     })
-  } catch (error) {
+    const { pullRequests } = sonarCloudData.data
+    const prNumber = github.context.issue.number;
+    const PrDetailsPosition = pullRequests.findIndex(pr => pr.key === String(prNumber))
+    const PRDetails = pullRequests[PrDetailsPosition]
+    console.log(PRDetails.status.qualityGateStatus)
+    if (PRDetails.status.qualityGateStatus === "ERROR") {
+        throw new Error(`Sonarcloud status: FAILED, please check sonar cloud for more details`)
+    }
+    console.log(`Sonarcloud status: SUCCESS`);
+} catch (error) {
     core.setFailed(error.message);
-  }
+}
